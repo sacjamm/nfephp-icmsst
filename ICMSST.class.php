0d00e4db65fe13446a5289c0c0cfcee198a90694
Alisson Maciel <sac.jamm@gmail.com>
2016-10-28 05:05:55 -0300

    Update ICMSST.class.php

--- ICMSST.class.php
+++ ICMSST.class.php
@@ -3,53 +3,61 @@
 class ICMSST {
 
     //BASE ICMS INTER
-    private $ValordoProduto;
-    private $ValorDoFrete;
-    private $ValorSeguro;
-    private $ValorOutrasDespesas;
-    private $ValorDescontos;
-    private $BaseDoICMS;
+    public $ValordoProduto;
+    public $ValorDoFrete;
+    public $ValorSeguro;
+    public $ValorOutrasDespesas;
+    public $ValorDescontos;
+    public $BaseDoICMS;
     //VALOR ICMS INTER
-    private $AliquotaICMSInter;
-    private $ValorDoICMS;
+    public $AliquotaICMSInter;
+    public $ValorDoICMS;
     //BASE ICMS ST
-    private $MVA;
-    private $BaseDoICMSST;
-    private $AliquotaICMSIntra;
+    public $MVA;
+    public $BaseDoICMSST;
+    public $AliquotaICMSIntra;
     public $ValorICMSST;
 
     function __construct() {
         
     }
 
-    private function CalculaBaseICMS() {
-        $this->BaseDoICMS = ($this->ValordoProduto + $this->ValorDoFrete + $this->ValorSeguro + $this->ValorOutrasDespesas - $this->ValorDescontos);
-        return $this->BaseDoICMS;
-    }
-
-    private function CalculaValorICMS() {
-        $this->ValorDoICMS = $this->CalculaBaseICMS() * ($this->AliquotaICMSInter / 100);
-        return $this->ValorDoICMS;
-    }
-
-    private function CalculaBaseICMSST() {
-        $this->BaseDoICMSST = ($this->ValordoProduto + $this->ValorDoIPI + $this->ValorDoFrete + $this->ValorSeguro + $this->ValorOutrasDespesas - $this->ValorDescontos) * (1 + ($this->MVA / 100));
-        return $this->BaseDoICMSST;
-    }
-
-    public function CalculaValorICMSST($ValordoProduto, $MVA, $AliquotaICMSInter, $AliquotaICMSIntra, $ValorDoFrete = null, $ValorSeguro = null, $ValorOutrasDespesas = null, $ValorDescontos = null, $ValorDoIPI = null) {
+    public function CalculaBaseICMS($ValordoProduto, $ValorDoFrete = null, $ValorSeguro = null, $ValorOutrasDespesas = null, $ValorDescontos = null) {
         $this->ValordoProduto = $ValordoProduto;
-        $this->MVA = $MVA;
-        $this->AliquotaICMSInter = $AliquotaICMSInter;
-        $this->AliquotaICMSIntra = $AliquotaICMSIntra;
         $this->ValorDoFrete = $ValorDoFrete;
         $this->ValorSeguro = $ValorSeguro;
         $this->ValorOutrasDespesas = $ValorOutrasDespesas;
         $this->ValorDescontos = $ValorDescontos;
-        $this->ValorDoIPI = $ValorDoIPI;
 
-        $this->ValorICMSST = ($this->CalculaBaseICMSST() * ($this->AliquotaICMSIntra / 100) - $this->CalculaValorICMS());
+        $this->BaseDoICMS = ($this->ValordoProduto + $this->ValorDoFrete + $this->ValorSeguro + $this->ValorOutrasDespesas - $this->ValorDescontos);
+        return $this->BaseDoICMS;
+    }
+
+    public function CalculaValorICMS($AliquotaICMSInter) {
+        $this->AliquotaICMSInter = $AliquotaICMSInter;
+        $this->ValorDoICMS = $this->BaseDoICMS * ($this->AliquotaICMSInter / 100);
+        return $this->ValorDoICMS;
+    }
+
+    public function CalculaBaseICMSST($ValorDoIPI,$MVA) {
+        $this->ValorDoIPI = $ValorDoIPI;
+        $this->MVA = $MVA;
+        $this->BaseDoICMSST = ($this->ValordoProduto + $this->ValorDoIPI + $this->ValorDoFrete + $this->ValorSeguro + $this->ValorOutrasDespesas - $this->ValorDescontos) * (1 + ($this->MVA / 100));
+        return $this->BaseDoICMSST;
+    }
+//
+    public function CalculaValorICMSST($AliquotaICMSIntra) {
+        $this->AliquotaICMSIntra = $AliquotaICMSIntra;
+
+        $this->ValorICMSST = ($this->BaseDoICMSST * ($this->AliquotaICMSIntra / 100) - $this->ValorDoICMS);
         return $this->ValorICMSST;
+    }
+    
+    public function TotaldaNota(){
+        $this->ValordoProduto = $this->ValordoProduto + $this->ValorICMSST;
+        return $this->ValordoProduto;
     }
 
 }
+
+// Tributacao
